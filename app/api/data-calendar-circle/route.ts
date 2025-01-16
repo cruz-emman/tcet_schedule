d48da433da73ca0919cb94ed9b91900bc36e97fa
Emman Cruz <emmancruz2000@gmail.com>
2025-01-16 09:37:49 +0800

    add

--- app/api/data-calendar-circle/route.ts
+++ app/api/data-calendar-circle/route.ts
@@ -4,7 +4,11 @@ import { endOfDay, format, startOfDay } from "date-fns"
 
 export async function GET(request: Request) {
    try {
-    const getData = await db.appointment.findMany()
+    const getData = await db.appointment.findMany({
+      where: {
+         status: "approved"
+      }
+    })
 
     const events = getData.map((data) => ({ date: format(data.event_date, "yyyy-MM-dd") }))
     return Response.json(events)
